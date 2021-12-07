27758db5402d7f5510baf4d3e25e388927a0be3f
Carlin Mitchell <carlin.d.mitchell@gmail.com>
2021-12-07 12:38:14 -0600

    before using ejs layouts

--- app.js
+++ app.js
@@ -9,35 +9,63 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
 
 const port = 3000;
-let userAddedItems = ["First things first", "Second things second", "Third things third"];
-app.get("/", (req, res) => {
 
+// Home route-----------------------------------------------------
+let homeListItems = [];
+app.get("/", (req, res) => {
+    
 
     // Generate a string of todays date and pass it to the template
     let today = new Date();
     
     dateDisplayOptions = {
     weekday: 'long', 
-    year: 'numeric', 
+    // year: 'numeric', 
     month: 'long', 
     day: 'numeric'}
 
     let dateToday = today.toLocaleDateString("en-US", dateDisplayOptions);
     
     // send the rendered template to the requestor
-    res.render("list", {fullDate: dateToday, userAddedItems: userAddedItems});
+    res.render("list", {listTitle: dateToday, userAddedItems: homeListItems});
 });
 
 app.post("/", (req, res) => {
     let userInput = req.body.newItem;
-    userAddedItems.push(userInput);
-    
-    res.redirect('/');
+    if(req.body.list === "Work"){
+        workListItems.push(userInput);
+        res.redirect("/work");
+    } else {
+        homeListItems.push(userInput);
+        res.redirect('/');
+    }
 });
+
+
+// Work routes--------------------------------------------
+let workListItems = [];
+app.get('/work', (req,res) => {
+    
+    res.render("list", {listTitle:"Work List", userAddedItems: workListItems});
+
+app.post('/work', (req, res) => {
+    let userInput = req.body.newItem;
+    workListItems.push(userInput);
+    
+    res.redirect('/work');
+});
+});
+
+
+
+
+
+
 
 app.listen(port, () =>{
     console.log('Server started on port ' + port);
 });
+
 
 
 
